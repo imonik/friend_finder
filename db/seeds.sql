6b76a8e3e6e24c43ece114265d0819491438d2d7
iMonik <mk.santb@gmail.com>
2019-04-27 21:30:43 -0700

    Added seeds script.

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+use friend_finder_db;
+
+insert into questions (question) 
+values ('You enjoy vibrant social events with lots of people.'), 
+('You often spend time exploring unrealistic yet intriguing ideas.'), 
+('You often think about what you should have said in a conversation long after it has taken place.'),
+('People can rarely upset you.'),
+('You often rely on other people to be the ones to start a conversation and keep it going.'),
+('You rarely worry if you made a good impression on someone you met.'),
+('You are very affectionate with people you care about.'),
+('You have a careful and methodical approach to life.'),
+('In your opinion, it is sometimes OK to step on others to get ahead in life.'),
+('When it comes to making life-changing choices, you mostly listen to your heart rather than your head.');
