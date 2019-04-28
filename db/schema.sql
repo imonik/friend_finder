0a5683870f3798b4776686ac776be60d4b52dea7
iMonik <mk.santb@gmail.com>
2019-04-27 20:59:35 -0700

    Added database schema.

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+create database friend_finder_db;
+
+create table questions (
+	id int not null auto_increment,
+	question varchar(500) not null,
+	primary key (id)
+);
+
+create table friends (
+	id int not null auto_increment,
+	name varchar(50) not null,
+	picture_link varchar(500) not null,
+	primary key (id)
+);
+
+create table scores (
+	id int not null auto_increment,
+	question_id int not null,
+	friend_id int not null,
+	constraint foreign key (question_id) references questions(id),
+	constraint foreign key (friend_id) references friends(id),
+	primary key (id)
+);
